cd8b063d70c4917f17d6dc86cc56f42ba8c7bc2a
mdawlad29 <mdawladbdinfo@gmail.com>
2024-11-21 01:51:40 +0600

    README file update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div>
+  <!-- Logo -->
+  <div class="border-b border-gray-300 mb-[50px]">
+    <img
+      src="/src/assets/images/logo.png"
+      alt="Logo"
+      class="w-[300px] h-[38px] mx-auto py-[10px] object-contain"
+    />
+  </div>
+</div>
