7003c5bb50b77d63269f955285ca8d00279c32ae
mdawlad29 <mdawladbdinfo@gmail.com>
2024-11-21 03:02:05 +0600

    Header component design

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+<div
+  class="border-b border-gray-300 mb-[50px] flex justify-center items-center"
+>
+  <img
+    src={"/src/assets/images/logo.png"}
+    alt={"Logo"}
+    class="w-[300px] h-[38px] py-[10px] object-contain"
+  />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <div>
-  <!-- Logo -->
-  <div class="border-b border-gray-300 mb-[50px]">
-    <img
-      src="/src/assets/images/logo.png"
-      alt="Logo"
-      class="w-[300px] h-[38px] mx-auto py-[10px] object-contain"
-    />
-  </div>
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores
+  laboriosam aut tempora maiores doloribus at. Rem sapiente repudiandae error
+  molestias, debitis, repellat dolores esse sit maxime nihil quis odio eaque?
 </div>
